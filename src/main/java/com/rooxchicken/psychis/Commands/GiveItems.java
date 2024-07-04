ba3cd80623b09b0b75d51f55e0e0013c034d6621
RooXChicken <rooxchicken@gmail.com>
2024-07-04 16:34:54 -0400

    Gives the player items

--- src/main/java/com/rooxchicken/psychis/Commands/GiveItems.java
+++ src/main/java/com/rooxchicken/psychis/Commands/GiveItems.java
@@ -28,10 +28,51 @@ public class GiveItems implements CommandExecutor
         
         ItemStack agniAxe = new ItemStack(Material.NETHERITE_AXE);
         ItemMeta agniMeta = agniAxe.getItemMeta();
-        agniMeta.setDisplayName("§xF§F§8§0§2§2§lAgni Axe");
+        agniMeta.setDisplayName("§x§F§F§8§0§2§2§lAgni Axe");
+        agniMeta.setCustomModelData(1);
         agniAxe.setItemMeta(agniMeta);
 
         player.getInventory().addItem(agniAxe);
+
+        ItemStack charger = new ItemStack(Material.CROSSBOW);
+        ItemMeta chargerMeta = charger.getItemMeta();
+        chargerMeta.setDisplayName("§x§F§F§F§3§3§9§lCharger");
+        chargerMeta.setCustomModelData(1);
+        charger.setItemMeta(chargerMeta);
+
+        player.getInventory().addItem(charger);
+
+        ItemStack voidPiercer = new ItemStack(Material.NETHERITE_AXE);
+        ItemMeta voidMeta = voidPiercer.getItemMeta();
+        voidMeta.setDisplayName("§x§C§C§3§9§F§F§lVoid Piercer");
+        voidMeta.setCustomModelData(2);
+        voidPiercer.setItemMeta(voidMeta);
+
+        player.getInventory().addItem(voidPiercer);
+
+        ItemStack frostFang = new ItemStack(Material.NETHERITE_SWORD);
+        ItemMeta frostMeta = frostFang.getItemMeta();
+        frostMeta.setDisplayName("§x§2§B§D§8§F§F§lFrostfang");
+        frostMeta.setCustomModelData(1);
+        frostFang.setItemMeta(frostMeta);
+
+        player.getInventory().addItem(frostFang);
+
+        ItemStack stormBringer = new ItemStack(Material.NETHERITE_SHOVEL);
+        ItemMeta stormMeta = stormBringer.getItemMeta();
+        stormMeta.setDisplayName("§x§D§9§E§E§F§3§lStormbringer");
+        stormMeta.setCustomModelData(1);
+        stormBringer.setItemMeta(stormMeta);
+
+        player.getInventory().addItem(stormBringer);
+
+        ItemStack tideShaper = new ItemStack(Material.NETHERITE_SWORD);
+        ItemMeta tideMeta = tideShaper.getItemMeta();
+        tideMeta.setDisplayName("§x§1§6§8§3§F§F§lTideshaper");
+        tideMeta.setCustomModelData(2);
+        tideShaper.setItemMeta(tideMeta);
+
+        player.getInventory().addItem(tideShaper);
 
         return true;
     }
